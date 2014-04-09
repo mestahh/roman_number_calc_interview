5a199ec3fa03fb9682d054d0e750d9600f0da694
bm <bm@basepointmedia.com>
2014-04-09 20:55:35 +0200

    Initial commit.

---
+++ test/roman_converter/RomanConverterTest.java
@@ -0,0 +1,14 @@
+package roman_converter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RomanConverterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
