f24f328c5ea817a1cb9a0cb711abb2c37ce221d0
bm <bm@basepointmedia.com>
2014-04-09 21:03:39 +0200

    Refactor test cases.

--- test/roman_converter/RomanConverterTest.java
+++ test/roman_converter/RomanConverterTest.java
@@ -2,15 +2,26 @@ package roman_converter;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class RomanConverterTest {
 
+	private RomanConverter testObj;
+
+	@Before
+	public void setUp() {
+		testObj = new RomanConverter();
+	}
+
 	@Test
 	public void it_should_convert_1_to_I() {
-		RomanConverter converter = new RomanConverter();
-		assertEquals(converter.convert("I"), 1);
-		assertEquals(converter.convert("II"), 2);
+		assertEquals(testObj.convert("I"), 1);
+	}
+
+	@Test
+	public void it_should_convert_2_to_II() {
+		assertEquals(testObj.convert("II"), 2);
 	}
 
 }
