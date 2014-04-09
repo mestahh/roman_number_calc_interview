f4b76d477bf2b442d44fce4cb61d8e532b5d3903
bm <bm@basepointmedia.com>
2014-04-09 20:57:02 +0200

    Not compiling test case.

--- test/roman_converter/RomanConverterTest.java
+++ test/roman_converter/RomanConverterTest.java
@@ -1,14 +1,15 @@
 package roman_converter;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class RomanConverterTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void it_should_convert_1_to_I() {
+		RomanConverter converter = new RomanConverter();
+		assertEquals(converter.convert(1), 1);
 	}
 
 }
