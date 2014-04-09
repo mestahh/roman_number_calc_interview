284845b7e51a3220d2510a29ab6f58fe571433d2
bm <bm@basepointmedia.com>
2014-04-09 21:37:44 +0200

    Refactor.

--- src/roman_converter/RomanConverter.java
+++ src/roman_converter/RomanConverter.java
@@ -1,6 +1,16 @@
 package roman_converter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanConverter {
+
+	public static Map<Character, Integer> values = new HashMap<Character, Integer>();
+	static {
+		values.put('V', 5);
+		values.put('X', 10);
+		values.put('I', 1);
+	}
 
 	public int convert(String romanNumber) {
 		int previous = 0;
@@ -8,13 +18,7 @@ public class RomanConverter {
 		int current = 0;
 
 		for (int i = 0; i < romanNumber.length(); i++) {
-			if (romanNumber.charAt(i) == 'V') {
-				current += 5;
-			} else if (romanNumber.charAt(i) == 'I') {
-				current++;
-			} else if (romanNumber.charAt(i) == 'X') {
-				current += 10;
-			}
+			current += values.get(romanNumber.charAt(i));
 
 			if (previous < current && previous != 0) {
 				current = current - previous;
