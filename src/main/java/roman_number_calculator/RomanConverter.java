aca3bd2dad187b5a82a34582586267280a793f0c
Balazs Mester <bmester@cloudera.com>
2021-03-25 07:44:04 +0100

    Add skeleton

---
+++ src/main/java/roman_number_calculator/RomanConverter.java
@@ -0,0 +1,8 @@
+package roman_number_calculator;
+
+public class RomanConverter {
+
+  public int convert(String romanNumber) {
+    return 0;
+  }
+}
---
+++ src/test/java/roman_number_calculator/RomanConverterTest.java
@@ -0,0 +1,13 @@
+package roman_number_calculator;
+
+import org.junit.Before;
+
+public class RomanConverterTest {
+
+  private RomanConverter testObj;
+
+  @Before
+  public void setUp() {
+    testObj = new RomanConverter();
+  }
+}
